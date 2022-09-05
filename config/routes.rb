e5c548976e5e7e0976561d4e597d999b2caa891f
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-05 14:32:45 -0700

    add about page & link footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get "/about", to: "about#index"
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -11,8 +13,6 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-
-  resources :about, only: [:index]
 
   namespace :admin do
     root to: 'dashboard#show'

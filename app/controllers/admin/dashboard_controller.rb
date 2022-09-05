160f1ed1db4570de6a33f5c52fceb5e475412999
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-05 11:54:39 -0700

    showing product & category on dash

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  
   def show
+    @counts = {
+      product: Product.count(:id),
+      category: Category.count(:id)
+    }
   end
 end

e4a9e2784ec62a9813488cc3186e286cdf9228e2
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-06 21:13:26 -0700

    add User validation, remove index, define controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
-  def index
+  def new
+    @user = User.new
   end
 
-  def new
+  def create
+    @user = User.create(user_params)
+      if @user.valid?
+        @user.save
+        redirect_to root
+      else
+        redirect :new
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
   end
 end

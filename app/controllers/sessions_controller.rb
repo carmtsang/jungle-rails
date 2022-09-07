2b8ebd593fca096d02d8d7efe992312a1a4bde54
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-07 12:31:44 -0700

    add controllers for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,15 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user
+        redirect_to root
+      else
+        redirect_to login_path
+      end
+  end
 
 end

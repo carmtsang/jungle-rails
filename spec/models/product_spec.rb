1df1c7c5125b1d2d82730c52819ec2ef6a64ee80
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-13 19:37:20 -0700

    add validations to check

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it 'should not be valid without a name' 
+    it "should not be valid without a price" 
+    it "should not be valid without a quantity"
+    it "should not be valid without a category"
+    
+  end
+end

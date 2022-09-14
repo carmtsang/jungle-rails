49bae1396bbca16f9dce34b03a9c46eae30842e4
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-13 20:42:57 -0700

    implement tests for validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,11 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-
-    it 'should not be valid without a name' 
-    it "should not be valid without a price" 
-    it "should not be valid without a quantity"
-    it "should not be valid without a category"
+    before(:each) do
+      @category = Category.create!(name: "Evergreen")
+      @product = Product.create!(name: "Alocasia", description: "blah blah", image: "blah", price_cents: 2999, quantity: 6, category_id: @category.id)
+    end
     
+    it 'should not be valid without a name' do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+    it "should not be valid without a price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+    it "should not be valid without a quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+    it "should not be valid without a category" do
+      @product.category = nil
+      expect(@product).to_not be_valid
+    end
+      
   end
 end

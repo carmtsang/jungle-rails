ed210b4717573f6002b26f0df1ab7400b2b3a00d
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-14 19:20:06 -0700

    update test to use errors.full_mesasges

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,24 +4,32 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     before(:each) do
       @category = Category.create!(name: "Evergreen")
-      @product = Product.create!(name: "Alocasia", description: "blah blah", image: "blah", price_cents: 2999, quantity: 6, category_id: @category.id)
+      @product = Product.new(name: "Alocasia", description: "blah blah", image: "blah", price_cents: 2999, quantity: 6, category_id: @category.id)
     end
     
     it 'should not be valid without a name' do
       @product.name = nil
-      expect(@product).to_not be_valid
+      @product.validate
+  
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
     it "should not be valid without a price" do
       @product.price_cents = nil
-      expect(@product).to_not be_valid
+      @product.validate
+
+      expect(@product.errors.full_messages).to include("Price is not a number")
     end
     it "should not be valid without a quantity" do
       @product.quantity = nil
-      expect(@product).to_not be_valid
+      @product.validate
+      
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
     it "should not be valid without a category" do
       @product.category = nil
-      expect(@product).to_not be_valid
+      @product.validate
+      
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
       
   end

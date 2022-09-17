096513ef5a1ad453e5d883961a5f306e722058d1
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-17 09:35:04 -0700

    add authenticate_with_credentials class method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
+        session[:user_id] = user.id
         redirect_to "/"
       else
         redirect_to login_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   validates :password, confirmation: true, length: { minimum: 6 }
   validates :first_name, :last_name, :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email)
+      if @user && @user.authenticate(password)
+        @user
+    elsif
+      puts "cant log in"
+      nil
+    end
+  end
+
 end
